237533cd6fadc91bb7c53de47c3fa2480b3b2d15
saurav <sauravdhr@gmail.com>
2019-11-06 15:47:28 -0500

    add copyright

--- tnet.py
+++ tnet.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
-# Here is the implementstion of TNet in python3
-# Saurav Dhar
+"""
+Copyright (C) 2019 Saurav Dhar (saurav.dhar@uconn.edu), Chengchen Zhang,
+Ion Mandoiu (ion.mandoiu@uconn.edu), and Mukul S. Bansal
+(mukul.bansal@uconn.edu).
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 # Library imports
 from Bio import Phylo
